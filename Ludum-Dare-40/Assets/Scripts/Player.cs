8127cb31bd3fa8e8d0428cf708e23dc7c2b04a79
John Pavlik <john.a.pavlik@gmail.com>
2017-12-01 23:54:42 -0500

    Orientation attempt

--- Ludum-Dare-40/Assets/Scripts/Player.cs
+++ Ludum-Dare-40/Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+    public float Engine = 10f;
+    public float Torque = 10f;
+
     void OnEnable() {
     }
 
@@ -32,9 +35,57 @@ public class Player : MonoBehaviour
             movement.x += 1;
         }
         Move(movement);
+
+        var worldMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Orient(worldMouse);
     }
 
     void Move(Vector2 vec) {
-        GetComponent<Rigidbody2D>().AddRelativeForce(vec);
+        GetComponent<Rigidbody2D>().AddRelativeForce(vec * Engine);
     }
+
+    void Orient(Vector2 vec) {
+        var body = GetComponent<Rigidbody2D>();
+        var angularVelocity = body.angularVelocity;
+        var distance = Vector2.SignedAngle(transform.right, vec);
+        if (angularVelocity == 0 && distance == 0) return;
+        var inertia = GetComponent<Rigidbody2D>().inertia;
+        var angularAcceleration = Torque / inertia;
+        var maxVelChange = angularAcceleration * Time.fixedDeltaTime;
+        var maxDisPerDelta = 0.5f * maxVelChange * Time.fixedDeltaTime;
+        if (Mathf.Abs(angularVelocity) < maxVelChange && Mathf.Abs(distance) < maxDisPerDelta)
+        {
+            body.MoveRotation(distance);
+            body.AddTorque(-body.angularVelocity * body.inertia, ForceMode2D.Impulse);
+            return;
+        }
+        if (Mathf.Abs(angularVelocity) <= Mathf.Epsilon && Mathf.Abs(distance) >= maxDisPerDelta)
+        {
+            body.AddTorque(Mathf.Sign(distance) * Torque);
+            return;
+        }
+
+        var timePerSpin = 360 / Mathf.Abs(angularVelocity);
+        if(Mathf.Abs(angularVelocity) > 0.5f * angularAcceleration * timePerSpin)
+        {
+            body.AddTorque(Mathf.Sign(angularVelocity) * -Torque);
+            return;
+        }
+
+        var timeToArrival = 2 * distance / angularVelocity;
+        if(timeToArrival < 0)
+        {
+            body.AddTorque(Mathf.Sign(distance) * Torque);
+            return;
+        }
+        var maxStopVel = angularAcceleration * timeToArrival;
+        if (Mathf.Abs(angularVelocity) > maxStopVel)
+        {
+            body.AddTorque(Mathf.Sign(distance) * -Torque);
+        } else
+        {
+            body.AddTorque(Mathf.Sign(distance) * Torque);
+        }
+    }
+    
 }
