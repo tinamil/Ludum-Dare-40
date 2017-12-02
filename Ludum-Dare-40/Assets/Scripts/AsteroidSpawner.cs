76efe0612d1f57a084cb113b3ce00e8c3e820a4a
John Pavlik <john.a.pavlik@gmail.com>
2017-12-02 18:08:15 -0500

    reduce bloom, fix asteroid collisions

--- Ludum-Dare-40/Assets/Scripts/AsteroidSpawner.cs
+++ Ludum-Dare-40/Assets/Scripts/AsteroidSpawner.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AsteroidSpawner : MonoBehaviour {
+public class AsteroidSpawner : MonoBehaviour
+{
 
     public int numAsteroids;
     public float radius;
     public GameObject[] Asteroids;
 
-	// Use this for initialization
-	void Start () {
-	
-        for(int i = 0; i < numAsteroids; ++i)
+    // Use this for initialization
+    void Start() {
+
+        for (int i = 0; i < numAsteroids; i++)
         {
             Vector3 position = Random.insideUnitCircle * radius;
-            Quaternion rotation = Random.rotation;
-            Instantiate(Asteroids[Random.Range(0, Asteroids.Length)], position, rotation);
+            float rotation = Random.Range(0, 360);
+            Instantiate(Asteroids[Random.Range(0, Asteroids.Length)], position, Quaternion.Euler(0, 0, rotation));
         }
-        
-	}
-	
+
+    }
+
 }
