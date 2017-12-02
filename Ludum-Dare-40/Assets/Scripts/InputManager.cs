87ac8b577af6a0f8171f44688ba949b491040ce0
John Pavlik <john.a.pavlik@gmail.com>
2017-12-01 22:55:26 -0500

    Working on player

---
+++ Ludum-Dare-40/Assets/Scripts/InputManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public enum InputEvent
+    {
+        Forward, Left, Right, Backward
+    }
+
+    static Dictionary<InputEvent, KeyCode> mapping;
+    static Dictionary<InputEvent, List<Action>> actions;
+
+    static public void AddAction(InputEvent evt, Action act) {
+        if (actions.ContainsKey(evt))
+        {
+            actions[evt].Add(act);
+        } else
+        {
+            actions.Add(evt, new List<Action>() { act });
+        }
+    }
+
+    static public void RemoveAction(InputEvent evt, Action act) {
+        if (actions.ContainsKey(evt))
+        {
+            actions[evt].Remove(act);
+        }
+    }
+
+    static public bool IsFiring(InputEvent evt) {
+        return Input.GetKey(mapping[evt]);
+    }
+
+    private void Awake() {
+        mapping = new Dictionary<InputEvent, KeyCode>();
+        actions = new Dictionary<InputEvent, List<Action>>();
+    }
+
+    private void OnEnable() {
+        mapping.Add(InputEvent.Forward, (KeyCode)PlayerPrefs.GetInt(InputEvent.Forward.ToString(), (int)KeyCode.W));
+        mapping.Add(InputEvent.Left, (KeyCode)PlayerPrefs.GetInt(InputEvent.Left.ToString(), (int)KeyCode.A));
+        mapping.Add(InputEvent.Right, (KeyCode)PlayerPrefs.GetInt(InputEvent.Right.ToString(), (int)KeyCode.D));
+        mapping.Add(InputEvent.Backward, (KeyCode)PlayerPrefs.GetInt(InputEvent.Backward.ToString(), (int)KeyCode.S));
+    }
+
+    // Update is called once per frame
+    void Update() {
+        foreach (var k in mapping)
+        {
+            if (Input.GetKeyDown(k.Value))
+            {
+                if (actions.ContainsKey(k.Key))
+                {
+                    actions[k.Key].ForEach(act => act.Invoke());
+                }
+            }
+        }
+    }
+}
---
+++ Ludum-Dare-40/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+    void OnEnable() {
+    }
+
+    void OnDisable() {
+
+    }
+
+    void Update() {
+        Vector2 movement = Vector2.zero;
+        if (InputManager.IsFiring(InputManager.InputEvent.Forward))
+        {
+            movement.y += 1;
+        }
+        if (InputManager.IsFiring(InputManager.InputEvent.Backward))
+        {
+            movement.y -= 1;
+        }
+        if (InputManager.IsFiring(InputManager.InputEvent.Left))
+        {
+            movement.x -= 1;
+        }
+        if (InputManager.IsFiring(InputManager.InputEvent.Right))
+        {
+            movement.x += 1;
+        }
+        Move(movement);
+    }
+
+    void Move(Vector2 vec) {
+        GetComponent<Rigidbody2D>().AddRelativeForce(vec);
+    }
+}
